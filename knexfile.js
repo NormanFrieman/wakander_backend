f16f7b48e4a5fcc5e2f698f760a97dc60cdc512b
NormanFrieman <ezequiel-d.s@hotmail.com>
2021-01-16 19:16:12 -0300

    new update

--- knexfile.js
+++ knexfile.js
@@ -19,7 +19,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: databaseConfig,
     migrations: {
       directory: __dirname + '/src/Database/migrations'
     },
